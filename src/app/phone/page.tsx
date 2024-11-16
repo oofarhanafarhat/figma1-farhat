4d4650d16c2947e251970f1658277ded9362fc33
oofarhanafarhat <khnkaneez@gmail.com>
2024-11-16 20:22:00 +0500

    Update page.tsx
    save changes

--- src/app/phone/page.tsx
+++ src/app/phone/page.tsx
@@ -4,16 +4,16 @@ import Link from 'next/link'
 
 const Ph = () => {
   return (
-    <div className=' w-auto mt-10 my-5  md:w-[1170px] md:h-[603px]  md:justify-center md:ml-10 opacity-100'>
+    <div className=' w-auto max-h-full mt-10 my-5  md:w-[1170px] md:h-[603px]  md:justify-center md:ml-10 opacity-100'>
         <h1 className='text-gray-400 text-opacity-100 text-3xl '>Contact Us</h1>
         <div className='flex justify-center '>
-            <div className=' w-auto h-auto flex flex-col sm:w-[391px] sm:h-[369px] space-y-3 '>
+            <div className=' w-auto h-full flex flex-col sm:w-[391px] sm:h-[369px] space-y-3 '>
                 <input type="text" placeholder='Name* ' className='bg-gray-300 text-opacity-100  py-5 rounded-lg pl-4 bg-opacity-100 mt-4'/>
               <input type="Email"  placeholder='Email* ' className='bg-gray-300 text-opacity-100 py-5 rounded-lg  pl-4 bg-opacity-100'/>
         <input type="text" placeholder='Interested IN ' className='bg-gray-300 text-opacity-100  py-5 rounded-lg pl-4 bg-opacity-100' />
             <input type="text"placeholder='message* ' className='bg-gray-300 text-opacity-100 pb-20  py-5 rounded-lg pl-4 bg-opacity-100' />
             </div>
-            <div className='w-aut0 h-369 md:w-[746] md:h-[369]'>
+            <div className='w-aut0 h-full md:w-[746] md:h-[369]'>
                < Image src="/image06.png" width={749}height={369}alt='' className=' w-auto h-369  md:w-[749] md:h-[369]'/> 
             </div>
         </div>
